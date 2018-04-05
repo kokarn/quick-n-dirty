696718260052dc0bfa5bd8c4a0c4ea0ab77ea9f4
Oskar Risberg <oskar.risberg@stendahls.se>
2018-04-05 15:25:53 +0200

    Pretty display of same score

--- web/scripts/local.js
+++ web/scripts/local.js
@@ -2,6 +2,7 @@
     var dataKey = '5ac4fcf9656b6e0b857c424e';
     var apiSecret;
     var dataInterval;
+
     var loadRecentData = function loadRecentData(){
         $.ajax( {
             url: 'https://api.jsonbin.io/b/' + dataKey + '/latest',
@@ -32,12 +33,28 @@
 
     var updateContent = function updateContent( recordData ){
         $( '.js-records-wrapper' ).empty();
+        var lastScore = 0;
+        var lastPosition = 0;
+
+        var position;
 
         for ( var i = 0; i < recordData.records.length; i = i + 1 ) {
-            $( '.js-records-wrapper' ).append( `<tr>
-                <td>${ i + 1 }</td>
-                <td>${ recordData.records[ i ].name }</td>
-                <td>${ recordData.records[ i ].score }</td>
+            position = i + 1;
+
+            if ( recordData.records[ i ].score === lastScore ) {
+                // position = lastPosition;
+                position = '-';
+                // position = '';
+            } else {
+                lastPosition = position;
+                lastScore = recordData.records[ i ].score;
+            }
+
+            $( '.js-records-wrapper' ).append( `
+                <tr>
+                    <td>${ position }</td>
+                    <td>${ recordData.records[ i ].name }</td>
+                    <td>${ recordData.records[ i ].score }</td>
                 </tr>
             ` );
         }
